428419c614f8273f5284d4825822ab98e92f363d
Jerry-Chang975 <jerry975607@gmail.com>
2023-06-05 00:26:09 +0800

    DP: LPS add state reduce

--- DP/longest_palindrome_subsequence.py
+++ DP/longest_palindrome_subsequence.py
@@ -1,4 +1,4 @@
-# 最長回文子序列
+# 最長回文子序列 time complexity O(N^2)
 def LPS(s: str):
     # define DP table: DP[i][j] = LPS of s[i:j]
     # solution is DP[0][len(s)]
@@ -16,6 +16,28 @@ def LPS(s: str):
                 DP[i][j] = max(DP[i][j-1], DP[i+1][j])
     
     return DP[0][len(s)-1]
+
+# 進一步狀態壓縮，DP 2d->1d
+def LPS_2(s: str):
+    # define DP table: DP[i][j] = LPS of s[i:j]
+    # solution is DP[len(s)-1]
+
+    # i = j -> len(s) = 1 -> LPS = 1
+    # 2d 映射到 row
+    DP = [1 for j in range(len(s))]
+    
+    for i in reversed(range(len(s)-1)):
+        pre = 0
+        for j in range(i+1, len(s)):
+            # 先把目前個 DP[j] 值存起來
+            temp = DP[j]
+            if s[i] == s[j]:
+                DP[j] = pre + 2 # pre = 上一個 DP[j]
+            else:
+                DP[j] = max(DP[j-1], DP[j])
+            pre = temp # 更新上一個 DP[j]
+    
+    return DP[len(s)-1]
 
 
 if __name__ == "__main__":
